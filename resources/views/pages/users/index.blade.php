66984466bf97ed1edbb55902a7e9b602e150c46e
fazrithe <fazrithe@gmail.com>
2023-04-11 16:48:09 +0700

    avatar

--- resources/views/pages/users/index.blade.php
+++ resources/views/pages/users/index.blade.php
@@ -25,7 +25,11 @@
                 <div class="box">
                     <div class="flex flex-col lg:flex-row items-center p-5 border-b border-slate-200/60 dark:border-darkmode-400">
                         <div class="w-24 h-24 lg:w-12 lg:h-12 image-fit lg:mr-1">
+                            @if($user->photo == null)
+                            <img alt="Icewall Tailwind HTML Admin Template" class="rounded-full" src="{{ asset('dist/images/' . $fakers[0]['photos'][0]) }}">
+                            @else
                             <img alt="Icewall Tailwind HTML Admin Template" class="rounded-full" src="{{ $user->photo }}">
+                            @endif
                         </div>
                         <div class="lg:ml-2 lg:mr-auto text-center lg:text-left mt-3 lg:mt-0">
                             <a href="" class="font-medium">{{ $user->name }}</a>
